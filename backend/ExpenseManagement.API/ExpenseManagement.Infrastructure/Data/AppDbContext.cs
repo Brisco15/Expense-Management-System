4a32b8f22a3b400c2da1273661c41edd64eef7e8
Brice Basse <bbasse0@gmail.com>
2026-05-19 12:39:09 +0200

    Created AppDbContext

---
+++ backend/ExpenseManagement.API/ExpenseManagement.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ExpenseManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseManagement.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Expense> Expenses => Set<Expense>();
+        public DbSet<Category> Categories => Set<Category>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+     
+        }
+    }
+}
