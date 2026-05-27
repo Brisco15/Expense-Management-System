39be1ca9e44ef68596e4af2715dc7b24739b7e8e
Brice Basse <bbasse0@gmail.com>
2026-05-27 11:45:03 +0200

    Created DashboardService.cs -implemented GetDashboardSummary -implemented GetMonthlyExpenses -implemented GetCategoryExpenses

--- backend/ExpenseManagement.API/ExpenseManagement.Infrastructure/Services/DashboardService.cs
+++ backend/ExpenseManagement.API/ExpenseManagement.Infrastructure/Services/DashboardService.cs
@@ -10,6 +10,57 @@ namespace ExpenseManagement.Infrastructure.Services
 {
     public class DashboardService : IDashboardService
     {
+        private readonly AppDbContext _context;
 
+        public DashboardService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<DashboardSummaryDto> GetDashboardSummaryAsync()
+        {
+           
+            return new DashboardSummaryDto
+            {
+                TotalExpenses =  await _context.Expenses.SumAsync(e => e.Amount),
+
+                TotalExpenseCount = await _context.Expenses.CountAsync(),
+
+                ApprovedExpenses = await _context.Expenses
+                                    .Where(e => e.Status == ExpenseStatus.Approved)
+                                    .SumAsync(e => e.Amount),
+
+                PendingExpenses = await _context.Expenses
+                                    .Where(e => e.Status == ExpenseStatus.Pending)
+                                    .SumAsync(e => e.Amount)
+
+            };
+        }
+
+        public async Task <List<MonthlyExpenseDto>> GetMonthlyExpensesAsync()
+        {
+            return await _context.Expenses.GroupBy(e => new
+                {
+                     e.ExpenseDate.Year,
+                     e.ExpenseDate.Month
+                })
+                .Select(g => new MonthlyExpenseDto
+                {
+                    Month = g.Key.Year + "-" + g.Key.Month,
+                    TotalAmount = g.Sum(e => e.Amount)
+                }).ToListAsync();
+        }
+
+        public async Task<List<CategoryExpenseDto>> GetCategoryExpensesAsync()
+        {
+            return await _context.Expenses
+                .Include(e => e.Category)
+                .GroupBy(e => e.Category!.Name)
+                .Select(g => new CategoryExpenseDto
+                {
+                    Category = g.Key,
+                    TotalAmount = g.Sum(e => e.Amount)
+                }).ToListAsync();
+        }
     }
 }
