919f240bfaf10355c97adb71d80042d8a2793b95
Brice Basse <bbasse0@gmail.com>
2026-05-27 11:12:43 +0200

    Update IExpenseService and add DashboardService
    Updated the IExpenseService interface:
    - Changed the return type of GetAllExpensesAsync to Task<List<ExpenseDto>>.
    - Added a new method DeleteExpenseAsync.
    
    Added a new DashboardService class:
    - Implements the IDashboardService interface.
    - Includes necessary using directives but lacks implemented logic.

--- backend/ExpenseManagement.API/ExpenseManagement.Application/Interfaces/IExpenseService.cs
+++ backend/ExpenseManagement.API/ExpenseManagement.Application/Interfaces/IExpenseService.cs
@@ -4,11 +4,12 @@ namespace ExpenseManagement.Application.Interfaces
 {
     public interface IExpenseService
     {
-        Task<ExpenseDto> GetAllExpensesAsync();
+        Task<List<ExpenseDto>> GetAllExpensesAsync();
         Task<ExpenseDto?> GetExpenseByIdAsync(int id);
         Task<ExpenseDto> CreateExpenseAsync(
             CreateExpenseDto createExpenseDto,
             int userId
             );
+        Task<bool> DeleteExpenseAsync(int id);
     }
 }
---
+++ backend/ExpenseManagement.API/ExpenseManagement.Infrastructure/Services/DashboardService.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseManagement.Application.DTOs;
+using ExpenseManagement.Application.Interfaces;
+using ExpenseManagement.Domain.Entities;
+using ExpenseManagement.Domain.Enums;
+using ExpenseManagement.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ExpenseManagement.Infrastructure.Services
+{
+    public class DashboardService : IDashboardService
+    {
+
+    }
+}
