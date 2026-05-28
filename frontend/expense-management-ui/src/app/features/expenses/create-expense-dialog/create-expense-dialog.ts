9ecbb41893893bada7a3acbf24e995ff289802b4
Brice Basse <bbasse0@gmail.com>
2026-05-28 13:45:47 +0200

    Add main layout, navbar, sidebar, login, register, dashboard home, create expense dialog, expense list, and user management components with associated styles and tests

---
+++ frontend/expense-management-ui/src/app/features/expenses/create-expense-dialog/create-expense-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-expense-dialog',
+  imports: [],
+  templateUrl: './create-expense-dialog.html',
+  styleUrl: './create-expense-dialog.css',
+})
+export class CreateExpenseDialog {}
