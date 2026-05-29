724d5ee227c32bc430507a799e04d1ddb73a5205
Brice Basse <bbasse0@gmail.com>
2026-05-29 10:55:10 +0200

    Add AuthResponse interface and import login/register models in auth service

--- frontend/expense-management-ui/src/app/core/services/auth.ts
+++ frontend/expense-management-ui/src/app/core/services/auth.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from '../../../environment';
-
+import { LoginRequest } from '../models/login.model';
+import { RegisterRequest } from '../models/register.model';
 
 @Injectable({
   providedIn: 'root',
