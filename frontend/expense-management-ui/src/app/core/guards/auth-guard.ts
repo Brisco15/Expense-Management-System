7623fbd9a70c2a26b55d87f658fca142a81271d7
Brice Basse <bbasse0@gmail.com>
2026-05-29 12:26:04 +0200

    Added auth guard to include dependency injection for AuthService and Router

--- frontend/expense-management-ui/src/app/core/guards/auth-guard.ts
+++ frontend/expense-management-ui/src/app/core/guards/auth-guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+ 
+  const isLogged = authService.isLoggedIn();
+
+  if(!isLogged){
+    router.navigateByUrl('/auth/login')
+    return false
+  }
+  
   return true;
 };
