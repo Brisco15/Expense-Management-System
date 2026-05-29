bf31d4e81b80ec74e478373c5669b81d3a64005f
Brice Basse <bbasse0@gmail.com>
2026-05-29 12:08:28 +0200

    JWT interceptor to use HttpInterceptorFn and streamline app configuration

--- frontend/expense-management-ui/src/app/app.config.ts
+++ frontend/expense-management-ui/src/app/app.config.ts
@@ -2,14 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { JwtInterceptor } from './core/interceptors/jwt-interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './core/interceptors/jwt-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
-
+    provideHttpClient(withInterceptors([jwtInterceptor]))
   ]
 };
--- frontend/expense-management-ui/src/app/core/interceptors/jwt-interceptor.ts
+++ frontend/expense-management-ui/src/app/core/interceptors/jwt-interceptor.ts
@@ -1,23 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { AuthService } from '../services/auth';
 
-@Injectable()
-export class JwtInterceptor implements HttpInterceptor {
-  constructor (private authService: AuthService){}
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.getToken();
 
-  intercept(req:HttpRequest<any>, next: HttpHandler){
-    const token = this.authService.getToken();
-
-    if(token){
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-    }
-    return next.handle(req);
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   }
 
-  
+  return next(req);
 };
