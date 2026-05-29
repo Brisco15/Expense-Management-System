64afebaf084547f0741683282cb5d4d973e76904
Brice Basse <bbasse0@gmail.com>
2026-05-29 14:29:30 +0200

    Add routing configuration for authentication and dashboard access

--- frontend/expense-management-ui/src/app/app.routes.ts
+++ frontend/expense-management-ui/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { DashboardHome } from './features/dashboard/dashboard-home/dashboard-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+
+    },
+    {
+        path: 'auth/login',
+        component: Login
+    },
+    {
+        path: 'auth/register',
+        component: Register
+    },
+    {
+        path: 'dashboard',
+        component: DashboardHome,
+        canActivate: [authGuard]
+    }
+];
