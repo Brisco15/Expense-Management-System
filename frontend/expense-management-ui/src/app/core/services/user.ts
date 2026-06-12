34d3ac62d436d3b48bc998f2974b8006ef406df6
Brice Basse <bbasse0@gmail.com>
2026-06-12 19:10:22 +0200

    Implement UserService with CRUD operations for user management

--- frontend/expense-management-ui/src/app/core/services/user.ts
+++ frontend/expense-management-ui/src/app/core/services/user.ts
@@ -1,6 +1,31 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environment';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class User {}
+export class UserService {
+  private apiUrl = `${environment.apiUrl}/user`;
+
+  constructor(
+    private http: HttpClient,
+  ){}
+
+  getAllUsers(){
+    return this.http.get<User[]>(this.apiUrl);
+  }
+
+  getUser(id: number){
+    return this.http.get<User>(`${this.apiUrl}/${id}`)
+  }
+
+  // updateUser(id: number, ){
+  //   return this.http.put<User>(`${this.apiUrl}/${id}/role`)
+  // }
+
+  deleteUser(id: number){
+    return this.http.delete<void>(`${this.apiUrl}/${id}`)
+  }
+}
